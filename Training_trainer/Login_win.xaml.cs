ba97779b0a8c1c051aacc5dbfe700c309b93325d
Никита Полуденный <nik_i_ta98@mail.ru>
2019-05-18 13:46:51 +0300

    Main_win и выход из него

--- Training_trainer/Login_win.xaml.cs
+++ Training_trainer/Login_win.xaml.cs
@@ -10,19 +10,49 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Training_trainer
 {
 	/// <summary>
-	/// Логика взаимодействия для MainWindow.xaml
+	/// Логика взаимодействия для Login_win.xaml
 	/// </summary>
-	public partial class MainWindow : Window
+	public partial class Login_win : Window
 	{
-		public MainWindow()
+		public Login_win()
 		{
 			InitializeComponent();
+		}
+
+		private void Tb_log_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			b_ent.IsEnabled = tb_log.Text != "" && tb_pass.Password != "";
+		}
+
+		private void Tb_pass_PasswordChanged(object sender, RoutedEventArgs e)
+		{
+			b_ent.IsEnabled = tb_log.Text != "" && tb_pass.Password != "";
+		}
+
+		private void B_ent_Click(object sender, RoutedEventArgs e)
+		{
+			if (/*Неверный логин*/tb_log.Text != "trainer")
+			{
+				MessageBox.Show("Пользователя с таким логином не существует", "Ошибка авторизации", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+			}
+			else
+			{
+				if (/*Неверный пароль*/tb_pass.Password != "0000")
+				{
+					MessageBox.Show("Пара логин-пароль не совпадают", "Ошибка авторизации", MessageBoxButton.OK, MessageBoxImage.Warning);
+				}
+				else
+				{
+					Main_win main = new Main_win(this);
+					main.Show();
+					Hide();
+				}
+			}
 		}
 	}
 }
---
+++ Training_trainer/Main_win.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Training_trainer
+{
+	/// <summary>
+	/// Логика взаимодействия для Main_win.xaml
+	/// </summary>
+	public partial class Main_win : Window
+	{
+		private bool logout { set; get; }
+		public Login_win super { get; }
+		public Main_win(Login_win super)
+		{
+			InitializeComponent();
+			this.super = super;
+		}
+
+		private void B_logout_Click(object sender, RoutedEventArgs e)
+		{
+			logout = true;
+			Close();
+		}
+
+		private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+			if (logout)
+			{
+				if (MessageBox.Show("Вы действительно хотите выйти?", "Выйти?", MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No) == MessageBoxResult.Yes)
+				{
+					super.tb_log.Clear();
+					super.tb_pass.Clear();
+					super.Show();
+				}
+				else
+				{
+					logout = false;
+					e.Cancel = true;
+				}
+			}
+			else
+			{
+				if (MessageBox.Show("Вы действительно хотите закрыть приложение?", "Закрыть приложение?", MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No) == MessageBoxResult.Yes)
+					Application.Current.Shutdown();
+				else
+					e.Cancel = true;
+			}
+		}
+	}
+}
