0215a7edbd22cdb218d1e0540809fb3ae92986ee
Никита Полуденный <nik_i_ta98@mail.ru>
2019-05-19 16:40:47 +0300

    Работа с профилем (начало)

---
+++ Training_trainer/Profile_win.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Training_trainer
+{
+	/// <summary>
+	/// Логика взаимодействия для Profile_win.xaml
+	/// </summary>
+	public partial class Profile_win : Window
+	{
+		public Profile_win()
+		{
+			InitializeComponent();
+		}
+	}
+}
