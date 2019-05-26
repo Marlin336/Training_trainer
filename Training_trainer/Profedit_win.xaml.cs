e0b0df742668bd6cdfd6bf6b8e464fd69386f956
Никита Полуденный <nik_i_ta98@mail.ru>
2019-05-26 23:51:45 +0300

    Расширение возможностей редактирования профиля

--- Training_trainer/Profedit_win.xaml.cs
+++ Training_trainer/Profedit_win.xaml.cs
@@ -28,7 +28,7 @@ namespace Training_trainer
 			this.super = super;
 			prof_win = profile;
 			InitializeComponent();
-			string sql = "select login, pass, mail from my_own_trainer(" + super.user_id + ")";
+			string sql = "select login, pass, mail, fname, sname, pname from my_own_trainer(" + super.user_id + ")";
 			NpgsqlCommand comm = new NpgsqlCommand(sql, super.conn);
 			try
 			{
@@ -39,6 +39,9 @@ namespace Training_trainer
 				tb_pass.Password = reader.GetString(1);
 				tb_repass.Password = reader.GetString(1);
 				tb_mail.Text = reader.GetValue(2).ToString();
+				tb_fname.Text = reader.GetString(3);
+				tb_sname.Text = reader.GetString(4);
+				tb_pname.Text = reader.GetString(5);
 			}
 			catch (NpgsqlException ex)
 			{
@@ -66,7 +69,7 @@ namespace Training_trainer
 					{
 						throw new ArgumentNullException();
 					}
-					string sql = "UPDATE trainer SET e_mail = '" + tb_mail.Text + "', login = '" + tb_login.Text + "', pass = '" + tb_pass.Password + "' WHERE id = " + super.user_id;
+					string sql = "UPDATE trainer SET first_name = '" + tb_fname.Text + "', second_name = '" + tb_sname.Text + "', parent_name = '" + tb_pname.Text + "', e_mail = '" + tb_mail.Text + "', login = '" + tb_login.Text + "', pass = '" + tb_pass.Password + "' WHERE id = " + super.user_id;
 					NpgsqlConnection conn = new NpgsqlConnection("Server=127.0.0.1;Port=5432;User Id=Training_login;Password=0000;Database=Training;");
 					NpgsqlCommand comm = new NpgsqlCommand(sql, conn);
 					conn.Open();
@@ -81,6 +84,7 @@ namespace Training_trainer
 					conn.Close();
 					prof_win.l_login.Content = "Логин: " + tb_login.Text;
 					prof_win.l_mail.Content = "E-mail: " + tb_mail.Text;
+					prof_win.l_name.Content = tb_sname.Text + " " + tb_fname.Text + " " + tb_pname.Text;
 					Close();
 				}
 				catch (NpgsqlException ex)
