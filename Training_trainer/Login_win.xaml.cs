b37ec2b4c461381d086facf93a3a7a1336ad92fd
Никита Полуденный <nik_i_ta98@mail.ru>
2019-06-04 14:13:18 +0300

    try_catch логина

--- Training_trainer/Login_win.xaml.cs
+++ Training_trainer/Login_win.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Npgsql;
+using System;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -24,30 +25,33 @@ namespace Training_trainer
 		}
 		private void B_ent_Click(object sender, RoutedEventArgs e)
 		{
-			string conn_param = "Server=127.0.0.1;Port=5432;User Id=Training_login;Password=0000;Database=Training;";
-			string sql = "select login_trainer('" + tb_log.Text + "', '" + tb_pass.Password + "')";
-			NpgsqlConnection conn = new NpgsqlConnection(conn_param);
-			NpgsqlCommand comm = new NpgsqlCommand(sql, conn);
-			conn.Open();
-			int result = (int)comm.ExecuteScalar();
-			conn.Close();
-			if (result == -1)
+			try
 			{
-				MessageBox.Show("Пользователя с таким логином не существует", "Ошибка авторизации", MessageBoxButton.OK, MessageBoxImage.Asterisk);
-			}
-			else
-			{
-				if (result == -2)
+				string conn_param = "Server=127.0.0.1;Port=5432;User Id=Training_login;Password=0000;Database=Training;";
+				string sql = "select login_trainer('" + tb_log.Text + "', '" + tb_pass.Password + "')";
+				NpgsqlConnection conn = new NpgsqlConnection(conn_param);
+				NpgsqlCommand comm = new NpgsqlCommand(sql, conn);
+				conn.Open();
+				int result = (int)comm.ExecuteScalar();
+				conn.Close();
+				if (result == -1)
 				{
-					MessageBox.Show("Пара логин-пароль не совпадают", "Ошибка авторизации", MessageBoxButton.OK, MessageBoxImage.Warning);
+					MessageBox.Show("Пользователя с таким логином не существует", "Ошибка авторизации", MessageBoxButton.OK, MessageBoxImage.Asterisk);
 				}
 				else
 				{
-					Main_win main = new Main_win(this, result, tb_log.Text, tb_pass.Password);
-					main.Show();
-					Hide();
+					if (result == -2)
+					{
+						MessageBox.Show("Пара логин-пароль не совпадают", "Ошибка авторизации", MessageBoxButton.OK, MessageBoxImage.Warning);
+					}
+					else
+					{
+						Main_win main = new Main_win(this, result, tb_log.Text, tb_pass.Password);
+						main.Show();
+						Hide();
+					}
 				}
-			}
+			}catch (Exception ex) { MessageBox.Show(ex.Message); }
 		}
 		#endregion
 	}
